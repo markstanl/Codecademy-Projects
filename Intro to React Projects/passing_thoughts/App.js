5eb62a4fed380db27b685444be69bb40cae519e4
Mark Stanley <146277800+markstanl@users.noreply.github.com>
2024-02-08 17:36:10 -0600

    Create App.js

---
+++ Intro to React Projects/passing_thoughts/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { AddThoughtForm } from './AddThoughtForm';
+import { Thought } from './Thought';
+import { generateId, getNewExpirationTime } from './utilities';
+
+export default function App() {
+
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: 'This is a place for your passing thoughts.',
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
+  function addThought(newThought){
+    setThoughts(thoughts => [newThought, ...thoughts]);
+  };
+
+const removeThought = (thoughtIdToRemove) => {
+  setThoughts((thoughts) =>
+    thoughts.filter((thought) => thought.id !== thoughtIdToRemove)
+  );
+};
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Passing Thoughts</h1>
+      </header>
+      <main>
+        <AddThoughtForm addThought={addThought}/>
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought key={thought.id} thought={thought}
+            removeThought={removeThought} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
