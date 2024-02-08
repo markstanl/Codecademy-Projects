42b0441ac50b70f2f7da4373df243f40465c7316
Mark Stanley <146277800+markstanl@users.noreply.github.com>
2024-02-08 17:35:17 -0600

    Create Contacts.js

---
+++ Intro to React Projects/authorization_form/Contacts.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function Contact() {
+  const password = 'swordfish';
+  const [authorized, setAuthorized] = useState(false);
+
+  function handleSubmit(e) {
+    const enteredPassword = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = enteredPassword == password;
+    setAuthorized(auth)
+  }
+
+  const login =(
+    <form action='#' onSubmit={handleSubmit}>
+      <input type='password' placeholder='Password' />
+      <input type='submit' />
+    </form>
+  )
+
+  let contactinfo =(
+        <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>
+  );
+
+  return (
+      <div id="authorization">
+        <h1>{authorized ? "Contact" : "Enter the Password"}</h1>
+        {authorized ? contactinfo : login}
+      </div>
+  );
+}
+
+export default Contact;
